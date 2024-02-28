4ccc276cc84c1f970fd1143b74d0452020b905c5
shivang-16 <shivangyadav121@gmail.com>
2024-02-29 01:40:33 +0530

    update multer

--- src/backend/middlewares/multer.ts
+++ src/backend/middlewares/multer.ts
@@ -1,28 +1,9 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(null, new Date().toISOString() + "-" + file.originalname);
-  },
-});
+const storage = multer.memoryStorage();
 
-// const fileFilter = (req, file, cb) => {
-//   if (
-//     file.mimetype === "image/jpeg" ||
-//     file.mimetype === "image/png" ||
-//     file.mimetype === "image/jpg"
-//   ) {
-//     cb(null, true);
-//   } else
-//     ({ error: "Unsupported file format. Upload only JPEG/JPG or PNG" }, false);
-// };
-
-const upload = multer({
+const singleUpload = multer({
   storage,
-//   limits: { fieldSize: 1024 * 1024 },
-});
+}).single("file");
 
-export default upload;
+export default singleUpload;
